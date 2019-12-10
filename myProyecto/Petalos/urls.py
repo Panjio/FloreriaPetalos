0e51c87207c04c01eed3212e349808c643512ccc
Francisco y Pablo <franromanc12@gmail.com>
2019-12-09 22:42:32 -0300

    carga

---
+++ myProyecto/Petalos/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('login/',login,name='LOGIN'),
+    path('cerrar_session/',cerrar_session,name='LOGOUT'),
+    path('',menu,name='MENU'),
+    path('formulario/',formulario,name='FORMU'),
+    path('carrito/',carrito,name='CARRI'),
+    path('agregar_carro/<id>/',carro_compras,name='AGREGAR_CARRO'),
+    path('carro_mas/<id>/',carro_compras_mas,name='CARRO_MAS'),
+    path('carro_menos/<id>/',carro_compras_menos,name='CARRO_MENOS'),
+    path('grabar_carro/',grabar_carro,name='GRABAR_CARRO'),
+]
